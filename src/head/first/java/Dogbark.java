c7f21f3ffd007b259baba0e2c6a36b4bcbe57fe9
Alexandr <photoluxe@mail.ru>
2017-03-23 00:29:45 +0600

    Getter&Setter

--- src/head/first/java/Dogbark.java
+++ src/head/first/java/Dogbark.java
@@ -7,13 +7,13 @@ public class Dogbark {
 
         //Создаем объекты собак присваиваем им имена и размер
         Dog dog1 = new Dog();
-        dog1.size = 70;
+        dog1.setSize(70);
 
         Dog dog2 = new Dog();
-        dog2.size = 35;
+        dog2.setSize(35);
 
         Dog dog3 = new Dog();
-        dog3.size = 8;
+        dog3.setSize(8);
 
         // Отправляем обьекты в метод реализующий лай собак и присваиваем количество гавканий
         dog1.bark("Рекс", 2);
@@ -23,18 +23,27 @@ public class Dogbark {
 
     private static class Dog {
 
-        public int size;
+        // Инкапсулируем переменную size, чтобы не обращаться к ней напрямую
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+
+        private int size;
 
         // Метод реализующий лай собак, в соответствии размерам собаки
         public void bark(String name, int i) {
-            if(size > 60) {
+            if(getSize() > 60) {
                 System.out.print(name + ":");
                 for (int j = 0; j < i; j++) {
                     System.out.print(" Гав гав!");
 
                 }
                 System.out.println("");
-            }else if(size > 14) {
+            }else if(getSize() > 14) {
                 System.out.print(name + ":");
                 for (int j = 0; j < i; j++) {
                     System.out.print(" Вуф вуф!");
